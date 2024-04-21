7a80e50a60cc1377f2a8f6a70b6a235eed2b188a
ender-osur <ender.ga10@gmail.com>
2024-04-20 21:19:04 -0500

    Taller 1. Ender Garizabal Sobrino. Corrección

--- class_uml/task_poo/encapsulamiento.py
+++ class_uml/task_poo/encapsulamiento.py
@@ -1,17 +1,14 @@
 class Relacion:
-    Mujer = "Sutanita"  # Accesible desde el exterior
-    __Moza = "Esperenzajita"  # No accesible
+    def __init__(self):
+        self.__mujer = "Sutanita"  # Realizamos encapsulación del atributo
+        self.__moza = "Esperenzajita"  # Realizamos encapsulación del atributo
 
-    # No accesible desde el exterior
-    def __llamar_moza_en_calle(self) -> None:
-        return f"Llamar a escondidas a {self.__Moza}"
+    def llama_mujer(self) -> str:
+        return f"Llamar normalmente a {self.__mujer}"
 
-
-    # Accesible desde el exterior
-    def llama_mujer(self) -> None:
-        # El método si es accesible desde el interior
-        return f"Llamar normalmente a {self.Mujer}"
+    def __llamar_moza_en_calle(self) -> str:
+        return f"Llamar a escondidas a {self.__moza}"
 
 final = Relacion()
 
-final.llama_mujer
+print(final.llama_mujer())
