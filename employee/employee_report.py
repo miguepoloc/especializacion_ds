ca58e3b7fc73f1358e8872421bbb67ff5cd08768
Cristian Charris <kristianschajris@gmail.com>
2024-09-28 02:08:43 +0000

    se crean las clases EmployeeReport y SalaryCalculator, para implementar el principio de responsabilidad unica

---
+++ employee/employee_report.py
@@ -0,0 +1,3 @@
+class EmployeeReport:
+    def generate_report(self, employee):
+        return f"Employee: {employee.name}, Hours Worked: {employee.hours_worked}, Hourly Rate: {employee.hourly_rate}"
---
+++ employee/salary_calculator.py
@@ -0,0 +1,3 @@
+class SalaryCalculator:
+    def calculate_salary(self, employee):
+        return employee.hours_worked * employee.hourly_rate
