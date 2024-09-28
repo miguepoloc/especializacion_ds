85606bce39cd628456ce27b27344965dd8e15bc9
Cristian Charris <kristianschajris@gmail.com>
2024-09-28 15:25:27 +0000

    se convierten todos los atributos de la clase a privados

--- employee/employee.py
+++ employee/employee.py
@@ -1,29 +1,35 @@
 class Employee:
     def __init__(self, name, hours_worked, hourly_rate):
-        self._name         = name
-        self._hours_worked = hours_worked
-        self._hourly_rate  = hourly_rate
+        self.__name = name
+        self.__hours_worked = hours_worked
+        self.__hourly_rate = hourly_rate
 
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @name.setter
     def name(self, value):
-        self._name = value
+        self.__name = value
 
     @property
     def hours_worked(self):
-        return self._hours_worked
+        return self.__hours_worked
 
     @hours_worked.setter
     def hours_worked(self, value):
-        self._hours_worked = value
+        self.__hours_worked = value
 
     @property
     def hourly_rate(self):
-        return self._hourly_rate
+        return self.__hourly_rate
 
     @hourly_rate.setter
     def hourly_rate(self, value):
-        self._hourly_rate = value
+        self.__hourly_rate = value
+
+    def update_hours_worked(self, additional_hours):
+        self.__hours_worked += additional_hours
+
+    def update_hourly_rate(self, new_rate):
+        self.__hourly_rate = new_rate
