033daa253ca750882fc595e0fc8caf4c7d8c7456
Cristian Charris <kristianschajris@gmail.com>
2024-09-29 04:21:24 +0000

    se realizan ajustes sin respetar la sustitucion de liskov

--- employe/main.py
+++ employe/main.py
@@ -6,14 +6,15 @@ def main():
     employee = Employee("Jose Perez", 40, 25, 0.10)
     salary_calculator = SalaryCalculator()
     report_generator = ReportGenerator()
+    bonnu_extra = 40
 
     calculated_salary_normal = salary_calculator.calculate_salary(employee)
     calculated_salary_annual = salary_calculator.calculate_annual_salary(employee)
-    calculated_salary_bonus = salary_calculator.calculate_bonus(employee)
+    calculated_salary_bonus = salary_calculator.calculate_bonus(employee) + bonnu_extra
 
 
     print(report_generator.generate_report(employee=employee, calculated_salary=calculated_salary_normal))
     print(report_generator.generate_annual_report(employee=employee, calculated_salary=calculated_salary_annual))
     print(report_generator.generate_bonus_report(employee=employee, calculated_salary=calculated_salary_bonus))
-    
+
 main()
--- employe/salary_calculator.py
+++ employe/salary_calculator.py
@@ -1,9 +1,11 @@
-class SalaryCalculator:
-	def calculate_salary(self, employee):
+from gnereic_salary_calculator import GenericSalaryCalculator
+
+class SalaryCalculator(GenericSalaryCalculator):
+	def calculate_salary(self, employee) -> float:
 		return employee.hours_worked * employee.hourly_rate
 
-	def calculate_annual_salary(self, employee):
+	def calculate_annual_salary(self, employee) -> float:
 		return self.calculate_salary(employee) * 12
 
-	def calculate_bonus(self, employee):
-		return self.calculate_salary(employee) * (1 + employee.bonus_rate)
+	def calculate_bonus(self, employee) -> float:
+		return str(self.calculate_salary(employee) * (1 + employee.bonus_rate))
